48211a9f51a4629a94355c3e37a1ce15611cf0e4
Rohit Agarwal <mindprince@gmail.com>
2013-05-31 21:22:28 +0530

    Make index.html serve as a static file. Now requests to www.mindprince.in/xyz will redirect to www.mindprince.in

--- main.py
+++ main.py
@@ -3,16 +3,10 @@
 import os
 
 import webapp2
-import jinja2
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'])
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
-		template = JINJA_ENVIRONMENT.get_template('index.html')
-		self.response.write(template.render())
+		self.redirect("/")
 
 app = webapp2.WSGIApplication([
     ('/.*', MainHandler),
