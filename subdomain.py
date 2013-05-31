9b7840b1e2b7360a52e1479d5ed177c3876037aa
Rohit Agarwal <mindprince@gmail.com>
2013-05-31 13:52:23 +0530

    Add path forwarding for subdomain redirects.

--- subdomain.py
+++ subdomain.py
@@ -3,9 +3,16 @@
 import webapp2
 
 class Redirect(webapp2.RequestHandler):
-	def get(self, subdomain):
-		self.redirect("http://" + subdomain + ".mindprince.in")
+	def get(self, subdomain, path=""):
+		self.redirect("http://" + subdomain + ".mindprince.in/" + path)
 
 app = webapp2.WSGIApplication([
+    ('/(blog)/(.*)', Redirect),
+    ('/(notes)/(.*)', Redirect),
     ('/(.*)', Redirect),
 ], debug=True)
+
+# Notes on routing
+# /blog matches the third rule.
+# /blog/ matches the first rule.
+# /blog/anything matches the first rule.
