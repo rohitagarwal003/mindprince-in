ce06ea1fd444e89b3610c61e94dd3eebf0a6515a
Rohit Agarwal <agarwalrohit@google.com>
2020-10-31 18:59:31 -0700

    Reduce redirects.
    Go from www.mindprince.in/blah to https://www.rohitagarwal.org/ directly instead of going to https://www.rohitagarwal.org/blah first.
    Go from www.rohitagarwal.org/blah to https://www.rohitagarwal.org/ directly instead of going to https://www.rohitagarwal.org/blah first.

--- redirect-app/redirect.go
+++ redirect-app/redirect.go
@@ -7,7 +7,7 @@ import (
 )
 
 func redirect(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://www.rohitagarwal.org"+r.URL.Path, 301)
+	http.Redirect(w, r, "https://www.rohitagarwal.org/", 301)
 }
 
 func main() {
